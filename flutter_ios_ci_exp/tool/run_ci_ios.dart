8a2bcef57f563cb204151fb0e04aca2570df7514
alex <alex@tekartik.com>
2022-12-04 11:14:48 +0100

    feat: try building without codesign

--- flutter_ios_ci_exp/tool/run_ci_ios.dart
+++ flutter_ios_ci_exp/tool/run_ci_ios.dart
@@ -1,17 +1,19 @@
 import 'package:tekartik_build_menu_flutter/app_build_menu.dart';
 import 'package:process_run/shell_run.dart';
 
-import 'dart:io';
-
 import 'package:path/path.dart';
 
 var appTest1Path = join('.dart_tool', 'test', 'exp1');
 Future<void> main() async {
   var appPath = appTest1Path;
-  await deleteDir(appPath);
-  var shell = Shell();
 
-  await shell.run('flutter create --template app --platforms ios --ios-language objc ${shellArgument(appPath)}');
+  var shell = Shell();
+  var create = true;
+  if (create) {
+    await deleteDir(appPath);
+    await shell.run(
+        'flutter create --template app --platforms ios --ios-language objc ${shellArgument(appPath)}');
+  }
   shell = shell.cd(appPath);
-  await shell.run('flutter build ios --debug');
+  await shell.run('flutter build ios --debug --no-codesign');
 }
